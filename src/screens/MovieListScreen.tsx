96d8b53d1b1f31ea2d3f3f4f5bed2608670c293b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 19:40:37 -0300

    feat: add text in movie screen

---
+++ src/screens/MovieListScreen.tsx
@@ -0,0 +1,21 @@
+import { View, StyleSheet, Text } from "react-native"
+
+const MovieListScreen = () => {
+ return(
+    <View
+        style ={styles.container}
+    >
+        <Text>MovieListScreen</Text>
+
+    </View>
+ )
+}
+
+export default MovieListScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    }
+})
