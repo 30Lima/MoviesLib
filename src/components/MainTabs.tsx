d587e2e93978c4fdb649d1f8fbc78f3d9dcd0f8f
30lima <pedrolimaa354@gmail.com>
2025-08-13 20:28:32 -0300

    feat: add components

--- src/components/MainTabs.tsx
+++ src/components/MainTabs.tsx
@@ -1,7 +1,53 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+import MovieListScreen from "../screens/MovieListScreen";
+import MapScreen from "../screens/MapScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+//<MaterialIcons name="movie" size={24} color="black" />
 
 const Tab = createBottomTabNavigator();
 
 export default function MainTabs() {
+    return(
+        <Tab.Navigator screenOptions={({ route }) => ({
+                tabBarActiveTintColor: "#EB4455",
+                tabBarInactiveTintColor: "#999",
+                tabBarLabelStyle: { fontSize: 12 },
+                tabBarIcon: ({ color,size }) => {
+                    let iconName: any;
+                    switch (route.name) {
+                        case "MovieListScreen":
+                            iconName = "movie";
+                            break;
+                        case "MapScreen":
+                            iconName = "map";
+                        case "SettingsScreen":
+                            iconName = "settings";
+                            break;
+                        default:
+                            iconName = "help";
+                    }
+                    return <MaterialIcons name={iconName} size={size} color={color}/>;
+                }
+            })}>
 
+                <Tab.Screen 
+                name="MovieListScreen"
+                component={MovieListScreen}
+                />
+
+                 <Tab.Screen 
+                name="MapScreen"
+                component={MapScreen}
+                />
+
+                 <Tab.Screen 
+                name="SettingsScreen"
+                component={SettingsScreen}
+                />
+
+        </Tab.Navigator>
+    )
 }
